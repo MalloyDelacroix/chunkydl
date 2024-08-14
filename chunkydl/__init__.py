cc9ce2fd16ba0bc96bfe830148997fed8348e471
Kyle <kyle.hickey222@gmail.com>
2024-08-14 10:58:37 -0500

    Add QueueDownloader to __all__

--- chunkydl/__init__.py
+++ chunkydl/__init__.py
@@ -1,7 +1,8 @@
 from .api import download, download_list
+from .queue_downloader import QueueDownloader
 from .config import DownloadConfig
 from .utils import DLGroup
 from .exceptions import RequestFailedException
 
 
-__all__ = ['download', 'download_list', 'DownloadConfig', 'DLGroup', 'RequestFailedException']
+__all__ = ['download', 'download_list', 'QueueDownloader', 'DownloadConfig', 'DLGroup', 'RequestFailedException']
